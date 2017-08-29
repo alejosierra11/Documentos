c6f5ff322bad0fa2b53f3edf196d13a98c13de78
alejosierra11 <30811076+alejosierra11@users.noreply.github.com>
2017-08-28 21:52:26 -0500

    Create FiguraGeometrica.java

---
+++ src/estructurasdedatos/cap1/tiposdedatos/FiguraGeometrica.java
@@ -0,0 +1,30 @@
+package estructuradedate.cap1.tiposdedatos;
+public class FiguraGeometrica{
+	private Sting codigo;
+  	private String tipo;
+  	private String Nombre;
+  
+  public string getCodigo(){
+  return codigo;
+  }
+  
+  public void setCodigo(){
+  this.codigo=codigo;
+  }
+  
+  public String getTipo;(){
+  return tipo;
+  }
+  
+  public void setCodigo(){
+  this.tipo=tipo;
+  }
+  
+  public String getNombre(){
+  return nombre;
+  }
+  
+  public void setNombre(){
+  this.nombre=nombre;
+  }
+}
