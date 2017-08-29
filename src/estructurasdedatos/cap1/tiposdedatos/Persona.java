2bd4e92e81ded52dde3595a3fac3a89875f4fe31
alejosierra11 <30811076+alejosierra11@users.noreply.github.com>
2017-08-28 21:45:44 -0500

    Create Persona.java

---
+++ src/estructurasdedatos/cap1/tiposdedatos/Persona.java
@@ -0,0 +1,41 @@
+package estructuradedate.cap1.tiposdedatos;
+public class Persona{
+			private String nombre;
+  			private String cedula;
+  			private String telefono;
+  			private String direccion;
+  
+  public String getNombre(){
+  
+    return nombre;
+  }
+  
+  public void setNombre(String nombre){
+  this.nombre =nombre;
+  }
+  
+  public int cedula(){
+  
+    return cedula;
+  }
+  
+  public void setCedula(int cedula){
+  this.cedula=cedula;
+  }
+  
+  public int setTelefono(){
+  return telefono;
+  }
+  
+  public void setTelefono(int telefono){
+  this.telefono=telefono;
+  }
+  
+  public String setDireccion(){
+  return direccion;
+  }
+  
+  public void getDireccion(String direccion){
+    this.direccion=direccion;
+  }
+		}
