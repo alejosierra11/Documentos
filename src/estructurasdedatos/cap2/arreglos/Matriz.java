606f361512e25cf741cfd623942ef3d8bcdcc206
alejosierra11 <alejosierra1997@utp.edu.co>
2017-09-09 19:55:34 -0500

    Practica subida de datos

--- src/estructurasdedatos/cap2/arreglos/Matriz.java
+++ src/estructurasdedatos/cap2/arreglos/Matriz.java
@@ -10,5 +10,43 @@ package estructurasdedatos.cap2.arreglos;
  * @author tusk
  */
 public class Matriz {
+    private int fila;
+    private int columna;
+    int matriz[][]=new int [fila][columna];
     
+    public void nMatriz(int nfila, int ncolumna){
+    this.fila=nfila;
+    this.columna=ncolumna;
+    this.matriz=new int [nfila][ncolumna];
+    }
+    
+    public void insertar (int nfila,int ncolumna, int dato){
+        this.fila=nfila;
+        this.columna=ncolumna;
+        this.matriz[nfila][ncolumna]=dato;
+    }
+    public void eliminar(int nfila,int ncolumna){
+        this.fila=nfila;
+        this.columna=ncolumna;
+        this.matriz[nfila][ncolumna]=0;
+    }
+    
+    public void buscar(int dato){
+    for (int nfila=0; nfila<matriz.length; nfila++){
+        for(int ncolumna=0; ncolumna<matriz.length; ncolumna++){
+    if (this.matriz[nfila][ncolumna]==dato){
+        System.out.println("El dato esta en la posicion:"+nfila+ncolumna);
+            }
+          }
+        }
+    }
+    
+    public void listar(){
+    for (int x=0;x<matriz.length; x++){
+    for (int y=0; y<matriz[x].length; y++){
+    System.out.println(matriz[x][y]);
+    System.out.println(" ");
+    }
+    }
+    }
 }
--- src/estructurasdedatos/cap2/arreglos/Vector.java
+++ src/estructurasdedatos/cap2/arreglos/Vector.java
@@ -10,5 +10,77 @@ package estructurasdedatos.cap2.arreglos;
  * @author tusk
  */
 public class Vector {
+private int tamaño;
+private String[]vector;
+
+public void crearVector(int n){
+this.tamaño=n;
+this.vector=new String[this.tamaño];
+}
+
+public void settamaño(int t){
+this.tamaño=t;
+}
+public int gettamaño(){
+return tamaño;
+}
+public boolean eliminar(int x){
+this.vector=null;
+return true;
+}
+public void insertar(String n, int pos){
+this.vector[pos]=n;
+}
+
+public String[] listas(){
+for (int q=0;q<=tamaño; q++){
+System.out.println(vector[q]);
+}
+
+
+public void ordenarVec(int vector[]){
+for(int i = 0; i < vector.length - 1; i++)
+        {
+            for(int j = 0; j < vector.length - 1; j++)
+            {
+                if (vector[j] < vector[j + 1])
+                {
+                    int tmp = vector[j+1];
+                    vector[j+1] = vector[j];
+                    vector[j] = tmp;
+                }
+            }
+        }
     
+}
+
+public void buscarBinario(int pos, int vector[]){
+Integer posini=0;
+Integer posfin=vector.length-1;
+Integer poscentral = null;
+while(posini<=posfin){
+if (vector[poscentral]==pos){
+System.out.println("el dato esta en:"+posini+"y"+posfin);
+}
+else {if(pos<=vector[poscentral]){
+posfin=poscentral;
+}
+}
+}
+}
+public void actualizar(int pos,String n){
+if ((pos<tamaño)&&(pos>0)){
+this.vector[pos]=n;
+}
+else{
+System.out.println("tamaño del vector incorrecto");
+}
+}
+public void borrar(int pos){
+if ((pos<tamaño)&&(pos<0))
+    this.vector[pos]=null;
+}
+else {
+System.out.println("tamaño incorrecto");
+}
 }
