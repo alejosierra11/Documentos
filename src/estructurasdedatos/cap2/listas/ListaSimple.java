73ce0ca4c67b0ddb6949d2e7273e1973be60dc9f
alejosierra11 <alejosierra1997@utp.edu.co>
2017-09-14 21:43:38 -0500

    se hizo NodoSimple y ListaSimple

--- src/estructurasdedatos/cap2/listas/ListaSimple.java
+++ src/estructurasdedatos/cap2/listas/ListaSimple.java
@@ -5,10 +5,58 @@
  */
 package estructurasdedatos.cap2.listas;
 
-/**
- *
- * @author tusk
- */
+import estructurasdedatos.cap2.listas.NodoSimple;
+
 public class ListaSimple {
-    
+
+    private NodoSimple cabeza;
+    private NodoSimple cola;
+    private NodoSimple acutual;
+
+    public void crearlistasimple() {
+        this.cabeza = null;
+        this.cola = null;
+    }
+
+    public void insertar(int dato) {
+        NodoSimple nodo = new NodoSimple();
+        nodo.crearnodo();
+        nodo.setdato(dato);
+
+        if (this.cabeza == null && this.cola == null) {
+            this.cabeza = nodo;
+            this.cola = nodo;
+        } else {
+            cola.setsiguiente(nodo);
+            this.cola = nodo;
+        }
+    }
+
+    public void eliminar(int dato) {
+        if (cabeza.getdato() == dato) {
+            cabeza = cabeza.getsiguiente();
+
+        } else {
+            NodoSimple aux = cabeza;
+            while (aux.getsiguiente().getdato() != dato) {
+                aux = aux.getsiguiente();
+
+            }
+            NodoSimple siguiente = aux.getsiguiente().getsiguiente();
+            aux.setsiguiente(siguiente);
+        }
+
+    }
+
+    public void listar() {
+        if (!(this.cabeza == null && this.cola == null)) {
+            NodoSimple aux = cabeza;
+
+            int i = 0;
+            while (aux != null) {
+                System.out.println("El dato :" + "(" + aux.getdato() + ")" + " " + "esta en la posicion" + "(" + i + ")");
+                i++;
+            }
+        }
+    }
 }
--- src/estructurasdedatos/utiles/nodos/NodoSimple.java
+++ src/estructurasdedatos/utiles/nodos/NodoSimple.java
@@ -5,10 +5,29 @@
  */
 package estructurasdedatos.utiles.nodos;
 
-/**
- *
- * @author tusk
- */
-public class NodoSimple {
-    
+class NodoSimple {
+
+    private NodoSimple siguiente;
+    private int dato;
+
+    public NodoSimple getsiguiente() {
+        return siguiente;
+    }
+
+    public void setsiguiente(NodoSimple siguiente) {
+        this.siguiente = siguiente;
+    }
+
+    public int getdato() {
+        return dato;
+    }
+
+    public void setdato(int dato) {
+        this.dato = dato;
+    }
+
+    public void crearNodo() {
+        this.siguiente = null;
+        this.dato=0;
+    }
 }
