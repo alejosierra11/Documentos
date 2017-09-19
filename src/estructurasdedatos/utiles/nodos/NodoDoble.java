a0466981c6a96d2a52aafb00e3b9556c85883000
alejosierra11 <30811076+alejosierra11@users.noreply.github.com>
2017-09-18 21:35:54 -0500

    Update NodoDoble.java

--- src/estructurasdedatos/utiles/nodos/NodoDoble.java
+++ src/estructurasdedatos/utiles/nodos/NodoDoble.java
@@ -1,14 +1,53 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package estructurasdedatos.utiles.nodos;
-
 /**
  *
- * @author tusk
+ * @author utp
  */
-public class NodoDoble {
-    
+public class Nododoble {
+
+    private int dato;
+    private Nododoble cabeza;
+    private Nododoble cola;
+    private Nododoble Nododoble;
+
+    public Nododoble getSiguiente() {
+        return cabeza;
+    }
+
+    public void setSiguiente(Nododoble siguiente) {
+        this.Nododoble = siguiente;
+    }
+
+    public Nododoble getAnterior() {
+        return cola;
+    }
+
+    public void setAnterior(Nododoble anterior) {
+        this.Nododoble = anterior;
+    }
+
+    public int getdato() {
+        return dato;
+    }
+
+    public void setdato(int dato) {
+        this.dato = dato;
+    }
+
+    public void crearNodo() {
+        this.cabeza = null;
+        this.cola = null;
+        this.dato = 0;
+    }
+
+    public void setsiguiente(Nododoble siguiente) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    public Object getsiguiente() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    public void crearnodo() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
 }
