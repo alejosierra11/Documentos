7cb7bc8813206ca59a60dcc8e1a436e6f5e1a96a
alejosierra11 <alejosierra1997@utp.edu.co>
2017-09-24 18:57:11 -0500

    Pila

--- src/estructurasdedatos/cap2/listas/Pila.java
+++ src/estructurasdedatos/cap2/listas/Pila.java
@@ -5,10 +5,61 @@
  */
 package estructurasdedatos.cap2.listas;
 
-/**
- *
- * @author tusk
- */
+import java.util.Stack;
+
 public class Pila {
-    
+
+    String arr[];
+    int max;
+    int top;
+
+    public Pila() {
+
+        arr = new String[10];
+        top = 0;
+    }
+
+    public boolean empity() {
+        if (top == 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public void push(String str) {
+        //insertar datos a la pila
+        if (top < arr.length) {
+
+            arr[top] = str;
+            top++;
+        } else {
+            String tempo[] = new String[arr.length + 1];
+            for (int i = 0; i < arr.length; i++) {
+                tempo[i] = arr[i];
+            }
+            arr = tempo;
+            arr[top] = str;
+            top++;
+        }
+    }
+
+    public String peek() {
+        if (top > 0) {
+            return arr[top - 1];
+        } else {
+            return null;
+        }
+    }
+
+    public String pop() {
+        String temp;
+        if (top > 0) {
+            temp = arr[top - 1];
+            arr[top - 1] = null;
+            top--;
+        }
+        return temp;
+    }
+
 }
