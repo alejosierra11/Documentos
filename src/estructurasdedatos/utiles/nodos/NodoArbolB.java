5fa5ec7fa09e795f9fe9fee350dd6187e97289b2
alejosierra11 <30811076+alejosierra11@users.noreply.github.com>
2017-10-23 22:35:19 -0500

    Update NodoArbolB.java

--- src/estructurasdedatos/utiles/nodos/NodoArbolB.java
+++ src/estructurasdedatos/utiles/nodos/NodoArbolB.java
@@ -10,5 +10,53 @@ package estructurasdedatos.utiles.nodos;
  * @author tusk
  */
 public class NodoArbolB {
-    
+
+    private String dato;
+    private Nodo izdo;
+    private Nodo dcho;
+
+    public NodoArbolB(String dato) {
+        this.dato = dato;
+    }
+
+    public String getDato() {
+        return dato;
+    }
+
+    /**
+     * @param dato the dato to set
+     */
+    public void setDato(String dato) {
+        this.dato = dato;
+    }
+
+    /**
+     * @return the izdo
+     */
+    public Nodo getIzdo() {
+        return izdo;
+    }
+
+    /**
+     * @param izdo the izdo to set
+     */
+    public void setIzdo(Nodo izdo) {
+        this.izdo = izdo;
+    }
+
+    /**
+     * @return the dcho
+     */
+    public Nodo getDcho() {
+        return dcho;
+    }
+
+    /**
+     * @param dcho the dcho to set
+     */
+    public void setDcho(Nodo dcho) {
+        this.dcho = dcho;
+    }
+
 }
+
