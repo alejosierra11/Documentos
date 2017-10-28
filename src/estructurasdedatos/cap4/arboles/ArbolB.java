0a669de7469a37bd4b806324781ea338ddd7168e
alejosierra11 <30811076+alejosierra11@users.noreply.github.com>
2017-10-27 22:34:51 -0500

    Update ArbolB.java

--- src/estructurasdedatos/cap4/arboles/ArbolB.java
+++ src/estructurasdedatos/cap4/arboles/ArbolB.java
@@ -9,6 +9,87 @@ package estructurasdedatos.cap4.arboles;
  *
  * @author tusk
  */
-public class ArbolB{
+public class ArbolBinario {
 
+    public int dato;
+    public NodoArbolBinario raiz;
+
+    public void insertarNodo(int dato) {
+
+        NodoArbolBinario nuevoNodo = new NodoArbolBinario();
+        NodoArbolBinario cont;
+        nuevoNodo.setDato(dato);
+        cont = raiz;
+        int a = 0;
+
+        if (raiz == null) {
+            raiz = nuevoNodo;
+           
+        } else {
+            while (a == 0) {
+                if (nuevoNodo.getDato() > cont.getDato()) {
+                    if (cont.getNodoDerecho() == null) {
+                        cont.setNodoDerecho(nuevoNodo);
+                      a++;
+                    } else {
+                        cont = cont.getNodoDerecho();
+                    }
+                } else if (cont.getNodoIzquierdo() == null) {
+                    cont.setNodoIzquierdo(nuevoNodo);
+                    a++;
+                } else {
+                    cont = cont.getNodoIzquierdo();
+                }
+            }
+        }
+    }
+
+    public void buscarDato(NodoArbolBinario raiz, int dato) {
+        NodoArbolBinario datico = new NodoArbolBinario();
+        NodoArbolBinario cont;
+        datico.setDato(dato);
+        cont = raiz;
+        int a = 0;
+
+        if (datico.getDato() == cont.getDato()) {
+                   } else {
+            while (a == 0) {
+                if (datico.getDato() > cont.getDato()) {
+                    if (cont.getNodoDerecho() == null) {
+                        a++;
+                    } else if (cont.getNodoDerecho().getDato() == datico.getDato()) {
+                        
+                        a++;
+                    } else {
+                        cont = cont.getNodoDerecho();
+                    }
+                } else if (cont.getNodoIzquierdo() == null) {
+                    System.out.println("No se encontró el número " + dato);
+                    a++;
+                } else if (cont.getNodoIzquierdo().getDato() == datico.getDato()) {
+                   a++;
+                } else {
+                    cont = cont.getNodoIzquierdo();
+                }
+            }
+        }
+    }
+
+    public void preOrder(NodoArbolBinario raiz) {
+        if (raiz != null) {
+          }
+    }
+
+    public void inOrder(NodoArbolBinario raiz) {
+        if (raiz != null) {
+            preOrder(raiz.getNodoIzquierdo());
+             }
+    }
+
+    public void postOrder(NodoArbolBinario raiz) {
+        if (raiz != null) {
+            preOrder(raiz.getNodoIzquierdo());
+            preOrder(raiz.getNodoDerecho());
+            }
+    }
 }
