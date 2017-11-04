87da11fbe8e4352a44a55b117f813367635a8d01
alejosierra11 <30811076+alejosierra11@users.noreply.github.com>
2017-11-03 21:29:21 -0500

    Update Grafo.java
    grafo

--- src/estructurasdedatos/cap5/grafos/Grafo.java
+++ src/estructurasdedatos/cap5/grafos/Grafo.java
@@ -3,12 +3,17 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package estructurasdedatos.cap5.grafos;
-
-/**
- *
- * @author tusk
- */
+package grafo;
+import java.util.ArrayList;import java.util.List;
 public class Grafo {
-    
+    List<vertice> vertice = new ArrayList<vertice>();    
+    List<arco> arco = new ArrayList<arco>();
+    public void insertarVertice(String dato) {        
+        vertice nuevoVertice = new vertice();    
+    }
+    public void insertarArco(String dato) {        
+        arco nuevoArco = new arco();        
+        nuevoArco vertice1 = (vertice1);        
+        nuevoArco vertice2 = (vertice2);    
+    }
 }
